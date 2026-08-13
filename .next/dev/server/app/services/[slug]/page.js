8cb1ed59d45e0de0868cd6d069ed5a9a6f726053
Abhishek Kumar <codewithabhi01@gmail.com>
2026-08-13 20:17:01 +0530

    fixed search console

--- .next/dev/server/app/services/[slug]/page.js
+++ .next/dev/server/app/services/[slug]/page.js
@@ -1,6 +1,6 @@
 var R=require("../../../chunks/ssr/[turbopack]_runtime.js")("server/app/services/[slug]/page.js")
 R.c("server/chunks/ssr/node_modules_next_dist_1gpf3i3._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0o_f4wr._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__1pw4pn1._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_esm_1bj8o03._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_compiled_0d323sd._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_server_route-modules_app-page_0qo_rmc._.js")
